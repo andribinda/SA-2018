1503cdfcfb8ab9cb259e9ba751df032472c595b0
abinda <andri.binda@gmail.com>
2019-01-16 10:21:23 +0100

    php-files für db login erstellt / tests für externe JS-files

---
+++ includes/psl-config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("HOST", "localhost");     
+define("USER", "sa_mysql");     
+define("PASSWORD", "sa2018");     
+define("DATABASE", "SA2018");   
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE); 
+
+?>
