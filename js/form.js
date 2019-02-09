7727652ca81379c0a112aacb8b902632c457f6bb
Binda <andribinda@gmail.com>
2019-02-09 13:43:57 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -24,7 +24,7 @@ function formhash(form, password) {
           'p': $('#p').val(),
         },
          success: function(data) {
-            if (data === 'Login') {
+            if (data === 'userlogin') {
               window.location = '/user.php';
             } else {
               alert('Invalid Credentials');
